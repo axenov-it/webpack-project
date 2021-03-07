cc97812346e77b19c212e6bc82909d0a47743411
alex0735070005 <alex0735070005@gmail.com>
2021-03-07 12:56:05 +0200

    add public folder

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
 
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, "public"),
     compress: true,
     port: 9002,
   },
